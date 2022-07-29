30f9f5523377e5a95cfef72618da4bd419e4b066
Peter Nied <petern@amazon.com>
2022-07-29 17:54:14 +0000

    Attempt to write test that repros 1970
    Run with `./gradlew test --tests
    org.opensearch.security.support.Base64HelperTest.testUserSerializationSafe`
    
    Signed-off-by: Peter Nied <petern@amazon.com>

--- src/test/java/org/opensearch/security/support/Base64HelperTest.java
+++ src/test/java/org/opensearch/security/support/Base64HelperTest.java
@@ -16,14 +16,21 @@ import java.io.Serializable;
 import java.net.InetSocketAddress;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.regex.Pattern;
 
 import com.google.common.io.BaseEncoding;
 import org.junit.Assert;
 import org.junit.Test;
-
 import org.opensearch.OpenSearchException;
 import org.opensearch.action.search.SearchRequest;
+import org.opensearch.common.collect.List;
+import org.opensearch.security.user.AuthCredentials;
 import org.opensearch.security.user.User;
 
 import static org.opensearch.security.support.Base64Helper.deserializeObject;
@@ -73,6 +80,63 @@ public class Base64HelperTest {
     public void testUser() {
         User user = new User("user");
         Assert.assertEquals(user, ds(user));
+    }
+
+    /** This test spins up a thread to add entries to a map, rotating through all values  */
+    @Test
+    public void testUserSerializationSafe() {
+        final AtomicInteger removedUserCount = new AtomicInteger(0);
+        final AtomicBoolean keepRunning = new AtomicBoolean(true);
+        final ConcurrentMap<Integer, User> userMap = new ConcurrentHashMap<Integer, User>();
+        final int concurrentActions = 10; // Dial up/down for more collisions
+        final ExecutorService pool = Executors.newFixedThreadPool(concurrentActions);
+        final int usersSize = 100; // Dial up/down for smaller collision space and runtime.
+
+
+        pool.submit(() -> {
+            int i = 0;
+            while (keepRunning.get()) {
+                i++; if (i == usersSize) { i = 0; }
+                final String userName = "Megan" + i;
+                final User newUser = new User(
+                    userName,
+                    List.of("roleA", "roleB", "roleC" + (int)(Math.random()*100)),
+                    new AuthCredentials(userName, "BackendRole1", "BackendRole2", "BackendRole3"));
+                userMap.put(i, newUser);
+            }
+        });
+
+        Runnable pickUserDeleter = () -> {
+            while(keepRunning.get()) {
+                try {
+                    final int pickedUser = (int)(Math.random() * usersSize);
+                    final User user = userMap.get(pickedUser);
+                    if (user == null) {
+                        continue;
+                    }
+                    ds(user);
+                    userMap.remove(pickedUser);
+                    removedUserCount.getAndIncrement();
+                } catch (Exception e) {
+                    System.out.println(e);
+                    keepRunning.set(false);
+                }
+            }
+        };
+
+        for(int i = 0; i < concurrentActions - 1; i ++){
+            pool.submit(pickUserDeleter);
+        }
+
+        while(keepRunning.get()) {
+            try {
+                Thread.sleep(100);
+            } catch (Exception _e) {/* Ignored */}
+            if (removedUserCount.get() >= 1000*usersSize) {
+                keepRunning.set(false);
+            }
+        }
+        System.out.println("Final number was " + removedUserCount.get());
     }
 
     @Test
